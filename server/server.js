64da02d52e511395e945b7208ae09a53f89ecaa4
Sam Henderson <hendersonsam456@gmail.com>
2016-10-18 21:40:57 -0700

    set port

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ var serverHelpers = require('../lib/serverHelpers');
 var app = express();
 app.use(bodyParser());
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use('/', express.static('../client'));
 
 app.get('/images', serverHelpers.fetchImages);
@@ -12,7 +14,7 @@ app.post('/image', serverHelpers.saveImage);
 
 app.delete('/image', serverHelpers.deleteImage);
 
-app.listen(8080, function () {
-  console.log('Photo Outlet Server listening on port 8080!');
-});
+// app.listen(8080, function () {
+//   console.log('Photo Outlet Server listening on port 8080!');
+// });
 
