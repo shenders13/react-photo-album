926f8320f49dac8c19340761bf7ed316407f2aed
Sam Henderson <hendersonsam456@gmail.com>
2016-10-19 00:12:33 -0700

    debugger

--- db/config.js
+++ db/config.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 var serverHelpers = require('../lib/serverHelpers.js');
 if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
+  console.log('connecting to mongo using environmental variable: ', process.env.MONGODB_URI)
 } else {
+  console.log('connecting to local mongo')
   mongoose.connect('mongodb://localhost/photoApp');
 }
 
