26a2ca14cf648f2f4d3b7c78fb2f31e167e49401
Sam Henderson <hendersonsam456@gmail.com>
2016-10-18 17:51:06 -0700

    hard coded heroku MONGODB url in

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var serverHelpers = require('../lib/serverHelpers.js');
-mongoose.connect('');
+mongoose.connect('mongodb://heroku_0224dth3:sabqib512c8cb46gid7iljbs4j@ds061506.mlab.com:61506/heroku_0224dth3');
 
 var db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
