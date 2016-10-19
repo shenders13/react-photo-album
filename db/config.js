549fcfcb8cf5a172665c582e65647bf9966af7b0
Sam Henderson <hendersonsam456@gmail.com>
2016-10-18 21:35:22 -0700

    heroku

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var serverHelpers = require('../lib/serverHelpers.js');
-mongoose.connect('mongodb://localhost/photoApp');
+mongoose.connect('mongodb://heroku_h33nqrvn:tar07caj285k8t64819jgm3t0h@ds061506.mlab.com:61506/heroku_h33nqrvn');
 
 var db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
